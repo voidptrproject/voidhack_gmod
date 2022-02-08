0fd1fa486c8812db729887525cf63b268e01fbff
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2022-02-08 20:31:25 +0300

    Добавьте файлы проекта.

---
+++ gmod/gmod.cpp
@@ -0,0 +1,15 @@
+﻿#include <Windows.h>
+
+#include <thread>
+
+void entry_point() {
+
+}
+
+BOOL APIENTRY DllMain(HINSTANCE dll_instance, DWORD reason, LPVOID reversed) {
+	DisableThreadLibraryCalls(dll_instance);
+	if (reason == DLL_PROCESS_ATTACH) {
+		std::thread(entry_point).detach();
+	}
+	return TRUE;
+}
